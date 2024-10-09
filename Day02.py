bee7daa0d2b42d6f6d5d0fb85c0d5835bfe29c28
Kapil Dev Chaudhary <kapildevchaudhary067@gmail.com>
2024-10-09 20:42:29 +0545

    Added Day 02 code

---
+++ Day02.py
@@ -0,0 +1,75 @@
+#Day02:
+#Data Types
+
+
+print("123" + " " + "345") # it is as like concatenation
+
+#integer
+
+print(123+345) # it is as like addition
+
+
+# float
+# 3.14159
+
+# #boolean
+True
+False
+street_name = "Abbey Road"
+print(street_name[4] + street_name[7])
+
+num_char = len(input("what is your name?"))
+print("The length of your name is " + str(num_char) + "characters")
+
+a = 123
+a = str(123)
+a = float(123)
+print(type(a))
+
+print(70 + float("100.5"))
+print(len("hello"))
+
+print(type("hello")) # you can know simply the type of the data that you given
+print(type(12))
+print(type(12.12))
+print(type(True))
+int("123")
+print("123" + "456")
+
+print(int("123") + int("456"))
+
+name_of_the_person = input("what is your name?")
+length_of_the_name = len(name_of_the_person)
+print("The length of your name is:" + str(length_of_the_name)) #we can not concatenate the int and str data type together
+
+print(len(input("what is your name?")))
+
+print("my age is " + str(20))
+
+print(5+5)
+print(10-5)
+print(5*5)
+print(25/5)
+print(25//5)
+print(2**3)# 2 power 3
+
+# PEMDAS
+# ()
+# **
+# * or /
+# + or -
+
+print(3*3+3/3-3) # that precedence rule of stack data structure
+print(3*3/3+3-3)
+
+
+score = 0
+height = 1.65
+is_winning = True
+# print("your score is " + str(score))
+print(f"your score is = {score} your height is = {height} your winning is = {is_winning}") # an alternative way if mixing data types
+
+
+
+
+
